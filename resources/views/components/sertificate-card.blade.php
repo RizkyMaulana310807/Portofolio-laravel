d92a200a1f4bb63af2619eb2a1224cd5a8d0a75a
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2026-04-04 21:55:11 +0700

    style: fix about design minimalist

--- resources/views/components/sertificate-card.blade.php
+++ resources/views/components/sertificate-card.blade.php
@@ -1,16 +1,19 @@
-<div class="w-38 h-52 bg-red-500 flex flex-col justify-between">
+<div class="w-64 h-82 bg-red-500 flex flex-col justify-between">
     <div class="flex w-full p-4 items-center justify-center bg-green-500">
-        <img src="{{ asset('images/GL3804834821_certificate_page-0001.jpg') }}" alt="sertificate.jpg"
-            class="w-22 object-contain">
+        <div class="bg-red-500 relative rounded-md">
+            <img src="{{ asset('images/GL3804834821_certificate_page-0001.jpg') }}" alt="sertificate.jpg"
+                class="w-52 object-contain">
+            <i class="text-xs fas fa-circle-info absolute top-1 left-1 border-2 border-main rounded-full bg-bright text-accent"></i>
+        </div>
     </div>
     <div class="flex w-full bg-accent p-2">
-        <h1 class="text-xs">
+        <h1 class="text-xs font-bold">
             sertifikat gamelab
         </h1>
     </div>
-    <div class="flex w-full bg-indigo-500 flex-row p-2">
-        <a href="#" class="text-xs"><i class="fas fa-download"></i></a>
-        <a href="#" class="text-xs"><i class="fas fa-check-circle"></i></a>
+    <div class="flex w-full bg-indigo-500 flex-row p-2 justify-evenly">
+        <a href="#" class="text-xs flex items-center justify-center"><i class="px-1 fas fa-circle-arrow-down text-accent rounded-full text-xl"></i></a>
+        <a href="#" class="text-xs flex items-center justify-center"><i class="px-1 fas fa-check-circle text-accent rounded-full text-xl"></i></a>
     </div>
     <div class="flex w-full bg-blue-800 flex-col p-2">
         <p class="text-xs">di sahkan oleh : Gamelab</p>
