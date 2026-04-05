eff000102bf67af0ee8dbae6ab6aa20aab9d994a
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2026-04-05 19:31:49 +0700

    Testing kirim email

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+
+Route::get('/test-email', function() {
+    Mail::raw("ini email test dari Laravel", function ($mesasage){
+        $mesasage->to('rizkymaulana317b@gmail.com')->subject('test email');
+    });
+    return 'email di kirim cek inbox lo';
+});
 
 Route::get('/', function () {
     return view('home');
