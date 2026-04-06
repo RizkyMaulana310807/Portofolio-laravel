1cf89c563d6cc1776e727fd968461ecff89f5eae
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2026-04-06 20:28:17 +0700

    style: menambahkan style system log

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -7,7 +7,21 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function showDashboard(){
-        return view('admin.dashboard');
+    public function showDashboard()
+    {
+        $logs = [
+            [
+                'id' => 1,
+                'ip' => '12.12.12.12',
+                'url' => '/Admin',
+                'method' => 'POST',
+                'agent' => 'Chrome',
+                'is_bot' => false,
+                'severity' => 'CRITICAL',
+                'timestamp' => '12-12-2027:59:59:12'
+            ],
+        ];
+
+        return view('admin.dashboard', compact('logs'));
     }
 }
--- resources/views/partials/dashboard/analytics-overview.blade.php
+++ resources/views/partials/dashboard/analytics-overview.blade.php
@@ -78,12 +78,53 @@
 
     <!-- [SYSTEM LOG] -->
 
-    <div class="bg-bright flex w-full h-72 rounded-2xl">
-        <div class="flex w-full p-4">
-            <h1>[SYSTEM LOG]</h1>
+    <div class="bg-bright flex w-full min-h-72 rounded-2xl flex-col">
+        <div class="flex w-full px-8 py-6">
+            <h1 class="font-bold font-quicksand">[SYSTEM LOG]</h1>
         </div>
-        <div class="bg-yellow-500">
+        <div class="w-full px-4">
+            <table class="w-full">
+                <thead>
+                    <tr class="bg-primary text-main">
+                        <th class="p-3 text-left">ID</th>
+                        <th class="p-3 text-left">IP</th>
+                        <th class="p-3 text-left">URL ACCESSED</th>
+                        <th class="p-3 text-left">HTTP METHOD</th>
+                        <th class="p-3 text-left">USER AGENT</th>
+                        <th class="p-3 text-left">IS BOT</th>
+                        <th class="p-3 text-left">SEVERITY</th>
+                        <th class="p-3 text-left">TIMESTAMP</th>
+                    </tr>
+                </thead>
 
+                <tbody>
+                    @if (count($logs) > 0)
+                        @foreach ($logs as $log)
+                            <tr class="bg-bright shadow-sm rounded-lg">
+                                <td class="p-4">{{ $log['id'] }}</td>
+                                <td class="p-4">{{ $log['ip'] }}</td>
+                                <td class="p-4">{{ $log['url'] }}</td>
+                                <td class="p-4">{{ $log['method'] }}</td>
+                                <td class="p-4">{{ $log['agent'] }}</td>
+                                <td class="p-4">
+                                    {{ $log['is_bot'] ? 'True' : 'False' }}
+                                </td>
+                                <td class="p-4">
+                                    <span class="px-4 py-2 rounded-full font-bold
+                                        {{ $log['severity'] === 'CRITICAL' ? 'bg-cinnabar/45 text-cinnabar' : '' }}">
+                                        {{ $log['severity'] }}
+                                    </span>
+                                </td>
+                                <td class="p-4">{{ $log['timestamp'] }}</td>
+                            </tr>
+                        @endforeach
+                    @else
+                    <tr>
+                        <th>No Data avaliable</th>
+                    </tr>
+                    @endif
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
