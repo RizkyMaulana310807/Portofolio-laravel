5e766ec35ccb3c04814da5fcfe45828eda74c4f6
Rizky Maulana <rizkymaulana317b@gmail.com>
2026-04-20 17:56:28 +0700

    style: membuat layout tampilan resource management

---
+++ resources/views/components/resource-preview-card.blade.php
@@ -0,0 +1,39 @@
+<div class="bg-darker p-2 flex flex-col rounded-2xl gap-4 w-fit">
+    
+    <!-- image container -->
+    <div class="w-48 aspect-[4/3] overflow-hidden rounded-xl">
+        <img 
+            src="https://i.pinimg.com/736x/ea/af/36/eaaf361622479e82f8000d4f7d0ecef6.jpg" 
+            alt="Bocchi.jpg"
+            class="w-full h-full object-cover">
+    </div>
+
+    <!-- header -->
+    <div class="flex gap-4 flex-col border-b border-main">
+        <div>
+            <h2 class="font-quicksand text-base">Bocchi.jpg</h2>
+            <h3 class="font-quicksand text-sm">Hi-Res Image</h3>
+        </div>
+        <div class="flex flex-row justify-between items-center pb-2">
+            <p class="text-sm font-quicksand text-main">4.2MB</p>
+            <p class="text-sm font-quicksand text-main">2 Hours Ago</p>
+        </div>
+    </div>
+
+    <!-- action button -->
+    <div class="flex w-full flex-row justify-around">
+        <div class="p-0.5 rounded-md bg-darker hover:bg-bright transition-all duration-200 ease-in-out cursor-pointer" title="edit">
+            <i data-lucide="pencil"></i>
+        </div>
+        <div class="p-0.5 rounded-md bg-darker hover:bg-bright transition-all duration-200 ease-in-out cursor-pointer" title="remove">
+            <i data-lucide="trash-2"></i>
+        </div>
+        <div class="p-0.5 rounded-md bg-darker hover:bg-bright transition-all duration-200 ease-in-out cursor-pointer" title="preview">
+            <i data-lucide="eye"></i>
+        </div>
+        <div class="p-0.5 rounded-md bg-darker hover:bg-bright transition-all duration-200 ease-in-out cursor-pointer" title="more">
+            <i data-lucide="ellipsis"></i>
+        </div>
+    </div>
+
+</div>
